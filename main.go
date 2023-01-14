55b06a933908f95f76a629356281989565bf152d
helio.rodrigues <5Aa7^NG%Pg5jv*p>
2023-01-14 14:20:28 -0300

    add get-all endpoint with static content

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(writer http.ResponseWriter, request *http.Request) {
+	articles := Articles{
+		Article{Title: "Title one", Desc: "Desc one", Content: "Content one"},
+		Article{Title: "Title two", Desc: "Desc two", Content: "Content Two"},
+	}
+
+	fmt.Println("endpoint hit: All articles")
+
+	json.NewEncoder(writer).Encode(articles)
+}
 
 func homePage(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprint(writer, "Homepage endpoint hit")
@@ -12,6 +32,8 @@ func homePage(writer http.ResponseWriter, request *http.Request) {
 
 func handleRequest() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
+
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
