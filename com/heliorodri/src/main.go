e10f05c712ec82a83066910788edadddb1eb241f
helio.rodrigues <5Aa7^NG%Pg5jv*p>
2023-01-15 12:31:20 -0300

    add mux router and create get-by-id endpoint

--- com/heliorodri/src/main.go
+++ com/heliorodri/src/main.go
@@ -5,25 +5,46 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type Article struct {
+	Id      int    `json:"id"`
 	Title   string `json:"title"`
 	Desc    string `json:"desc"`
 	Content string `json:"content"`
 }
 
-type Articles []Article
+var articles = [4]Article{
+	{Id: 1, Title: "Title one", Desc: "Desc one", Content: "Content one"},
+	{Id: 2, Title: "Title two", Desc: "Desc two", Content: "Content Two"},
+	{Id: 3, Title: "Title three", Desc: "Desc three", Content: "Content three"},
+	{Id: 4, Title: "Title four", Desc: "Desc four", Content: "Content four"},
+}
 
 func allArticles(writer http.ResponseWriter, request *http.Request) {
-	articles := Articles{
-		Article{Title: "Title one", Desc: "Desc one", Content: "Content one"},
-		Article{Title: "Title two", Desc: "Desc two", Content: "Content Two"},
-	}
-
 	fmt.Println("endpoint hit: All articles")
 
 	json.NewEncoder(writer).Encode(articles)
+}
+
+func findArticleById(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	id := vars["id"]
+	int_id, error := strconv.Atoi(id)
+
+	if error != nil {
+		panic(error)
+	}
+
+	for _, article := range articles {
+		if article.Id == int_id {
+			json.NewEncoder(writer).Encode(article)
+		}
+	}
+
 }
 
 func homePage(writer http.ResponseWriter, request *http.Request) {
@@ -31,12 +52,17 @@ func homePage(writer http.ResponseWriter, request *http.Request) {
 }
 
 func handleRequest() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", allArticles)
+	myRouter := mux.NewRouter().StrictSlash(true)
 
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", allArticles)
+	myRouter.HandleFunc("/articles/{id}", findArticleById)
+
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func main() {
+	fmt.Println("Router - API v2 - Mux Router")
+
 	handleRequest()
 }
